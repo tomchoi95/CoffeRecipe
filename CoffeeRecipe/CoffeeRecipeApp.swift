3dd1f4d37f21ea7b353b42d4956f1580fbed12c5
Tom Bum Su Choi <qjatn0545@naver.com>
2025-01-15 10:27:26 +0900

    First Commit

---
+++ CoffeeRecipe/CoffeeRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeRecipeApp.swift
+//  CoffeeRecipe
+//
+//  Created by 최범수 on 2025-01-15.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
